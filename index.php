03c59211cccbb364a857b1fa246ea7f70df4d201
zibonbadi <zibonbadi@users.noreply.github.com>
2021-12-29 13:28:46 +0100

    Dummy GET queries

--- index.php
+++ index.php
@@ -6,11 +6,27 @@ use Klein\Klein;
 $router = new Klein();
 
 $testdata = [
-"host" => "localhost:5029",
-"game" => "Sonic Robo Blast 2",
-"version" => "2.2.9",
-"name" => "SRB2 Server",
-"meta" => "files=VCL_LegacyHinote-v3.5.wad,CL_Skip-v1.pk3,VL_BattleMod-v1.1.pk3;mod=Battlemod V1.1",
+    [
+	"host" => "localhost:5029",
+	"game" => "Sonic Robo Blast 2 Kart",
+	"version" => "1.3",
+	"name" => "Kart Server",
+	"meta" => "cheats",
+    ],
+    [
+	"host" => "localhost:5030",
+	"game" => "Sonic Robo Blast 2",
+	"version" => "2.2.9",
+	"name" => "SRB2 Server",
+	"meta" => "files=VCL_LegacyHinote-v3.5.wad,CL_Skip-v1.pk3,VL_BattleMod-v1.1.pk3;mod=Battlemod V1.1",
+    ],
+    [
+	"host" => "localhost:65535",
+	"game" => "Minecraft",
+	"version" => "1.18.1",
+	"name" => "MC server",
+	"meta" => "server=bukkit 1.16.1;gamemode=adventure;cheats",
+    ],
 ];
 
 $router->with('/servers', function() use ($router, $testdata) {
@@ -18,22 +34,57 @@ $router->with('/servers', function() use ($router, $testdata) {
     use ($testdata) {
       $response->header('Content-Type','text/csv;header=absent;charset=UTF-8');
 
+    $filter = array_intersect_key($request->params(), [
+	"host" => null,
+	"game" => null,
+	"version" => null,
+	"name" => null,
+	"meta" => null,
+	"count" => null,
+	"page" => null,
+    ]);
+
+      $use_pager = array_key_exists("count",$filter);
+      $page = (array_key_exists("page",$filter))?intval($filter["page"]):1;
+
       // Get response data
       $data = fopen("php://temp", 'w');
-      fputcsv($data, $testdata);
+      $viewcounter = 0;
+      foreach($testdata as $row){
+	  $match = true;
+	  foreach($filter as $f_key => $f_value){
+	      switch($f_key){
+		  case "name": // Contain-Match names for comfortable searches
+		  case "meta":
+		      {
+			  // toUpper to make searches more comfortable
+			  if( !str_contains( strtoupper($row[$f_key]), strtoupper($f_value) ) ){ $match = false; }
+			  break;
+		      }
+		  case "count":
+		  case "page":
+		      { break; } // Ignore pager
+		  default:{
+		      if($row[$f_key] != $f_value){ $match = false; }
+		      break;
+		  }
+	      }
+	  }
+	  if(!$match){ continue; } // Skip unmatching
+
+	  if($use_pager){
+	    if( $viewcounter >= intval($filter["count"])*($page-1) && 
+	        $viewcounter < intval($filter["count"])*$page 
+	    ){ 
+	    fputcsv($data, $row); }
+	  }else{ fputcsv($data, $row); }
+	  $viewcounter++;
+      }
       rewind($data);
 
-      // Generate response
-      return "GET recognized: "
-	  .var_export($request->params())
-	  ."\n".stream_get_contents($data);
+      // Return table
+      return stream_get_contents($data);
     });
-    /*
-    $router->respond('GET', '/[:id]', function($request) {
-      $response->header('Content-Type','text/csv;header=absent;charset=UTF-8');
-      return "GET recognized {$request->id}";
-    });
-    */
 
     $router->respond('POST', '/?', function($request, $response) {
       $response->header('Content-Type','text/csv;header=absent;charset=UTF-8');
@@ -76,6 +127,7 @@ $router->with('/auth', function() use ($router) {
     });
 });
 
+/*
 // Failsafe
 $router->respond(['GET', 'OPTIONS'], '/?[**:path]?', function($request, $response) {
    $response->header('Content-Type','text/plain');
@@ -91,6 +143,7 @@ $router->respond(['GET', 'OPTIONS'], '/?[**:path]?', function($request, $respons
    };
    return "Not a chaosnet route. Try /servers";
 });
+*/
 
 
 $router->dispatch();
